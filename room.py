57eb82591c34ad99f99817308f6ae6c27483c816
unknown <clement.mespouilles@gmail.com>
2025-10-21 18:52:43 +0200

    Ajout de quelques pièces pour simuler les premiers tirages

--- room.py
+++ room.py
@@ -23,11 +23,65 @@ class EntryHall(RoomObject):
 
 class Parlor(RoomObject):
     
-    def __init__(self, name, image):
-        super().__init__(name, image)
+    def __init__(self):
+        super().__init__("Parlor", "Images/Rooms/Parlor.png")
     
 
 class AnteChamber(RoomObject):
 
     def __init__(self):
         super().__init__("AnteChamber", "Images/Rooms/Antechamber.png") 
+
+
+class Aquarium(RoomObject):
+
+    def __init__(self):
+        super().__init__("Cloister", "Images/Rooms/Aquarium.png")
+
+
+class Attic(RoomObject):
+
+    def __init__(self):
+        super().__init__("Cloister", "Images/Rooms/Attic.png") 
+
+
+class Ballroom(RoomObject):
+
+    def __init__(self):
+        super().__init__("Cloister", "Images/Rooms/Ballroom.png")
+
+
+class Billiard_Room(RoomObject):
+
+    def __init__(self):
+        super().__init__("Cloister", "Images/Rooms/Billiard_Room.png") 
+
+
+class Boiler_Room(RoomObject):
+
+    def __init__(self):
+        super().__init__("Cloister", "Images/Rooms/Boiler_Room.png")
+
+
+class Chamber_of_Mirrors(RoomObject):
+
+    def __init__(self):
+        super().__init__("Cloister", "Images/Rooms/Chamber_of_Mirrors.png") 
+
+
+class Closet(RoomObject):
+
+    def __init__(self):
+        super().__init__("Cloister", "Images/Rooms/Closet.png") 
+
+
+class Coat_Check(RoomObject):
+
+    def __init__(self):
+        super().__init__("Cloister", "Images/Rooms/Coat_Check.png")
+
+
+class Conference_Room(RoomObject):
+
+    def __init__(self):
+        super().__init__("Cloister", "Images/Rooms/Conference_Room.png") 
