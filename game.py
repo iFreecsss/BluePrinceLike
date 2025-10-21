00a71db5b1826b9196e49410497ca821cad26495
unknown <clement.mespouilles@gmail.com>
2025-10-21 18:59:08 +0200

    Ajout d'un tirage aléatoire de 3 salles pour chaque nouveau déplacement

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 from player import *
 from map import *
 from room import *
+import random
 
 class Game:
 
@@ -9,18 +10,28 @@ class Game:
         self.map = Map()
         self.data = {}
 
+        # test implémentation tirage des pièces
+        # il y aura 2 modes. celui qui permet de se déplcacer librement 'EXPLORING' et celui qui oblige
+        #  à placer les pièces 'ROOM_DRAWING'
+        self.game_state = 'EXPLORING'
+        # stocke les 3 pièces à choisir
+        self.current_choice_index = 0
+        self.room_choices = []
+        # mémorise la position où placer la pièce choisie
+        self.pending_placement_position = None
+        # pioche de salles
+        self.room_deck = [
+            Aquarium, Attic, Ballroom, Billiard_Room, 
+            Boiler_Room, Chamber_of_Mirrors, Closet, 
+            Coat_Check, Conference_Room
+        ]
+
     def check_status(self):
         """
         Rof a voir
         """
         pass
 
-    def draw_room(self):
-        """
-        Hardcodé de façon à tester l'ajout de chambres sur l'ui
-        """
-        return Parlor()
-    
     def player_orientation(self, input):
         """
         Appel de fonction pour appeler une fontion dans player, pas ouf,
@@ -58,23 +69,52 @@ class Game:
         MIN_Y, MAX_Y = 0, 8
         
         if (MIN_X <= new_x <= MAX_X) and (MIN_Y <= new_y <= MAX_Y): # Vérifie les limites de la map
-            self.player.move(final_position)
-    
+
+            target_cell = self.map.get_current_mapping()[new_x, new_y]
+
+            if target_cell is None:
+                # si la case adjacente est vide on peut lancer le tirage
+                print(f"Case vide à {final_position}. Lancement du tirage.")
+                self.game_state = "DRAWING_ROOM"
+                self.pending_placement_position = final_position
+                self.draw_new_rooms()
+            else:
+                # si elle est déjà occupée avec une pièce on avance normalement
+                print(f"Déplacement vers la case occupée {final_position}")
+                self.player.move(final_position)
+            # rajouter les vérifications de collisions etc plus tard
+
+    def handle_room_selection(self, input):
+        """
+        Gère les inputs de la phase de sélection.
+        """
+        # même rôle que player movement mais pour la sélection de salle permet d'alléger un peu handle_inputs
+        if input == "LEFT_ROOM":
+            self.current_choice_index = (self.current_choice_index - 1) % 3 # permet de boucler (appuyer 1 fois sur droite revient à appuyer 2 fois sur gauche)
+        elif input == "RIGHT_ROOM":
+            self.current_choice_index = (self.current_choice_index + 1) % 3
+        elif input == "ENTER":
+            self.select_room_choice(self.current_choice_index)
+
     def handle_inputs(self, inputs):
-        #input types:
+        
+        if self.game_state == "EXPLORING":
+            direction_change=["UP","DOWN","LEFT","RIGHT"]
+            movement_confirmation = ["SPACE"]
 
-        #Direction
-        direction_change=["UP","DOWN","LEFT","RIGHT"]
-        #Movement_Confirmation
-        movement_confirmation = ["SPACE"]
+            for i in inputs:
+                if i in direction_change:
+                    self.player_orientation(i)
+                
+                if i in movement_confirmation:
+                    self.player_movement(i)
 
-        for i in inputs:
-            if i in direction_change:
-                self.player_orientation(i)
-            
-            if i in movement_confirmation:
-                self.player_movement(i)
-
+        elif self.game_state == "DRAWING_ROOM":
+            # marche de la même façon que pour l'exploration
+            for i in inputs:
+                if i in ["LEFT_ROOM", "RIGHT_ROOM", "ENTER"]:
+                    self.handle_room_selection(i)
+                    break
 
     def publish_data(self):
         """
@@ -83,12 +123,36 @@ class Game:
         self.data['position'] = self.player.position
         self.data['direction'] = self.player.direction
         self.data['mapping'] = self.map.get_current_mapping()
+        # on rajoute game state pour que UI sache quel mode afficher
+        self.data['game_state'] = self.game_state
+        # on rajoute room choices (celles que le joueur peut choisir) pour l'affichage de l'UI
+        self.data['room_choices'] = self.room_choices
+        # on rajoute l'index de la salle actuellement sélectionnée pour l'affichage de l'UI (contours rouges)
+        self.data['current_choice_index'] = self.current_choice_index
         return self.data
     
+    def draw_new_rooms(self):
+            """
+            Tire 3 salles au hasard depuis le deck et les stocke.
+            """
+            # aléatoire très rudimentaire, à améliorer plus tard pour éviter les doublons, les cartes spéciales, 
+            # pas toutes les cases payantes, les directions des cases bonnes ...
+            chosen_room_classes = random.sample(self.room_deck, 3)
+            self.room_choices = [RoomClass() for RoomClass in chosen_room_classes]
+            # pour l'affichage dans l'UI
+            self.data['room_choices'] = self.room_choices
+            # mis à 0 pour commencer le choix depuis la salle de gauche
+            self.current_choice_index = 0
+
+    def select_room_choice(self, choice_index):
+        # est appelée par handle_room_selection une fois que le joueur a fait son choix
+        # chosen _room prend la valeur de la salle choisie
+        chosen_room = self.room_choices[choice_index]
+        self.map.place_room(chosen_room, self.pending_placement_position)
+        self.player.move(self.pending_placement_position)
+        # une fois la salle placée on remet l'état du jeu en exploration
+        self.game_state = "EXPLORING"
+        # on vide la liste pour les prochaines tirages
+        self.room_choices = []
+        self.pending_placement_position = None
     
-
-
-    
-
-
-
