4d70af493d9a6a45eeda145f82f537e409e92dd1
iFreecsss <paul.camus@etu.sorbonne-universite.fr>
2025-10-30 10:58:00 +0100

    Logique pour restart button dans les settings

--- main.py
+++ main.py
@@ -8,4 +8,11 @@ game_logic = Game()
 while(True):
     game_interface.set_data(game_logic.publish_data()) 
     inputs = game_interface.run()
+
+    if "RESTART_GAME" in inputs:
+        print("--- RESTARTING GAME ---")
+        game_logic = Game()
+        game_interface = UI()
+        continue
+    
     game_logic.handle_inputs(inputs)
