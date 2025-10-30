05cd875fcc9872e22debb182efc935d171735732
iFreecsss <paul.camus@etu.sorbonne-universite.fr>
2025-10-30 10:57:29 +0100

    Modification de l'aléatoire pour toujours avoir une salle gratuite proposée. ATTENTION : pas d'inventaire encore donc pas d'implémentation du coût dans game.py

--- random_manager.py
+++ random_manager.py
@@ -15,7 +15,8 @@ class RandomManager:
         self.room_deck = [
             Aquarium, Attic, Ballroom, Billiard_Room, 
             Boiler_Room, Chamber_of_Mirrors, Closet, 
-            Coat_Check, Conference_Room, Parlor
+            Coat_Check, Conference_Room, Parlor, Security, 
+            Foyer, Kitchen, Dining_Room, Passageway, Master_Bedroom
         ]
         
     def is_room_placable(self, RoomClass, current_map, position, direction_of_entry):
@@ -54,21 +55,60 @@ class RandomManager:
         if not placable_room_classes:
             # Cas horrible aucune pièce n'est plaçable normalement ça ne devrait jamais arriver
             return []
-            
-        # Préparer le tirage pondéré en fonction de la rareté
-        weights = [
-            RARITY_WEIGHTS.get(RoomClass.rarity) 
-            for RoomClass in placable_room_classes
+        
+
+        placable_free_rooms = [
+            RoomClass for RoomClass in placable_room_classes 
+            if RoomClass.cost == 0
         ]
         
-        # Là j'utilise .choices pour faire un tirage avec remise donc on a possiblement des doublons dans la 
-        # même sélection de 3 pièces mais on peut utiliser .sample si on veut absolument pas de doublons
-        # mais j'iame bien l'idée d'avoir des doublons possibles comme ça les salles rares sont vraiment rares
-        chosen_classes = random.choices(
-            placable_room_classes, 
-            weights=weights, 
-            k=3
-        )
+        chosen_classes = []
 
-        # On retourne les 3 salles instanciées
+        if not placable_free_rooms:
+            # Si aucune pièce gratuite n'est dispo on tire juste 3 pièces normales pour éviter de crash
+            weights = [
+                RARITY_WEIGHTS.get(RoomClass.rarity) 
+                for RoomClass in placable_room_classes
+            ]
+            
+            # Là j'utilise .choices pour faire un tirage avec remise donc on a possiblement des doublons dans la 
+            # même sélection de 3 pièces mais on peut utiliser .sample si on veut absolument pas de doublons
+            # mais j'iame bien l'idée d'avoir des doublons possibles comme ça les salles rares sont vraiment rares
+            chosen_classes = random.choices(
+                placable_room_classes, 
+                weights=weights, 
+                k=3
+            )
+        else:
+            # on récupère les poids des pièces dont le cost=0
+            free_weights = [
+                RARITY_WEIGHTS.get(RoomClass.rarity) 
+                for RoomClass in placable_free_rooms
+            ]
+
+            # Pièce gratuite garantie (on en tire que 1)
+            guaranteed_free_room = random.choices(
+                placable_free_rooms, 
+                weights=free_weights, 
+                k=1
+            )
+            # .extend pour éviter de créer une liste dans une liste
+            chosen_classes.extend(guaranteed_free_room)
+            
+            # On récupère tous les poids des pièces quelque soit leur cost
+            all_weights = [
+                RARITY_WEIGHTS.get(RoomClass.rarity) 
+                for RoomClass in placable_room_classes
+            ]
+            # Parmi toutes les pièces (y compris les cost=0) on en tire 2 autres
+            other_rooms = random.choices(
+                placable_room_classes, 
+                weights=all_weights, 
+                k=2
+            )
+            chosen_classes.extend(other_rooms)
+            
+            # Mélange pour ne pas avoir la pièce gratuite toujours en première position
+            random.shuffle(chosen_classes)
+
         return [RoomClass() for RoomClass in chosen_classes]
